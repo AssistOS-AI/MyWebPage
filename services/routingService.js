e48d17df7ce2f02b0ba7e2e41a9fd28c8fde2965
Koroem <conduratvictor@yahoo.com>
2024-02-14 12:48:08 +0200

    Refactoring

--- services/routingService.js
+++ services/routingService.js
@@ -1,12 +1,6 @@
 export class routingService {
     constructor() {}
     async navigateToLocation(locationArray = [], appName) {
-        const MYWEBPAGE_PAGE = "my-web-page";
-
-        if (locationArray.length === 0 || locationArray[0] !== MYWEBPAGE_PAGE) {
-            console.error("Invalid URL structure: URL must start with 'my-web-page'");
-            return;
-        }
         const webComponentName = locationArray[locationArray.length - 1];
         const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${locationArray.join("/")}`;
         await webSkel.changeToDynamicPage(webComponentName, pageUrl);
