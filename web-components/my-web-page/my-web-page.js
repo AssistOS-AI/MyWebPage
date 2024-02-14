0965c59c6947f4838aa5d968606f7ba7702be818
Koroem <conduratvictor@yahoo.com>
2024-02-14 12:46:37 +0200

    Refactoring

--- web-components/my-web-page/my-web-page.js
+++ web-components/my-web-page/my-web-page.js
@@ -36,25 +36,25 @@ export class myWebPage {
     }
 
     async showActionBox(_target, primaryKey, componentName, insertionMode) {
-        await webSkel.UtilsService.showActionBox(_target, primaryKey, componentName, insertionMode);
+        await webSkel.showActionBox(_target, primaryKey, componentName, insertionMode);
     }
 
     getPageId(_target) {
-        return webSkel.UtilsService.reverseQuerySelector(_target, "page-unit").getAttribute("data-id");
+        return webSkel.reverseQuerySelector(_target, "page-unit").getAttribute("data-id");
     }
 
     async showAddPageModal() {
-        await webSkel.UtilsService.showModal(document.querySelector("body"), "add-page-modal", {presenter: "add-page-modal"});
+        await webSkel.showModal("add-page-modal", {presenter: "add-page-modal"});
     }
 
     async deleteAction(_target) {
         let flowId = webSkel.currentUser.space.getFlowIdByName("DeletePage");
-        await webSkel.getService("LlmsService").callFlow(flowId, this.getPageId(_target));
+        await webSkel.appServices.callFlow(flowId, this.getPageId(_target));
         this.invalidate();
     }
 
     async editAction(_target) {
-        await webSkel.UtilsService.showModal(document.querySelector("body"), "edit-my-web-page-modal", {
+        await webSkel.showModal("edit-my-web-page-modal", {
             presenter: "edit-my-web-page-modal",
             id: this.getPageId(_target)
         });
