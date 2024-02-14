b493cf26fa5473be750ab3482471d2de246df0b7
Koroem <conduratvictor@yahoo.com>
2024-02-14 12:53:58 +0200

    Refactoring

--- services/routingService.js
+++ services/routingService.js
@@ -1,6 +1,17 @@
 export class routingService {
     constructor() {}
     async navigateToLocation(locationArray = [], appName) {
+        const MYWEBPAGE_PAGE = "my-web-page";
+
+        if (locationArray.length === 0 || locationArray[0] === MYWEBPAGE_PAGE) {
+            const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${MYWEBPAGE_PAGE}`;
+            await webSkel.changeToDynamicPage(MYWEBPAGE_PAGE, pageUrl);
+            return;
+        }
+        if(locationArray[locationArray.length-1]!== MYWEBPAGE_PAGE){
+            console.error(`Invalid URL: URL must end with ${MYWEBPAGE_PAGE}`);
+            return;
+        }
         const webComponentName = locationArray[locationArray.length - 1];
         const pageUrl = `${webSkel.currentUser.space.id}/${appName}/${locationArray.join("/")}`;
         await webSkel.changeToDynamicPage(webComponentName, pageUrl);
